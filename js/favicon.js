a6593516fb03a96baebc08a45243481d3e72e105
Shubham Kadu <shubhamkadu887@gmail.com>
2025-03-08 16:40:15 +0530

    Images path updated

--- js/favicon.js
+++ js/favicon.js
@@ -1,12 +1,12 @@
-function changeFavicon(src) {
-    let link = document.querySelector("link[rel~='icon']");
-    if (!link) {
-        link = document.createElement("link");
-        link.rel = "icon";
-        document.head.appendChild(link);
-    }
-    link.href = src;
-}
+// function changeFavicon(src) {
+//     let link = document.querySelector("link[rel~='icon']");
+//     if (!link) {
+//         link = document.createElement("link");
+//         link.rel = "icon";
+//         document.head.appendChild(link);
+//     }
+//     link.href = src;
+// }
 
-// Example: Change favicon dynamically
-changeFavicon("../favicon.svg");
+// // Example: Change favicon dynamically
+// changeFavicon("../favicon.svg");
