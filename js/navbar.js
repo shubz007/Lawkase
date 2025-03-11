cb94b432ac61c61f9f6e0c6080a269e9ed69b447
Shubham Kadu <shubhamkadu887@gmail.com>
2025-03-11 13:59:52 +0530

    logo color to Golden

--- js/navbar.js
+++ js/navbar.js
@@ -2,7 +2,7 @@
   window.addEventListener("scroll", function () {
     let navbar = document.querySelector(".site-navbar");
     if (window.scrollY > 150) {
-        navbar.style.background = "rgba(128, 128, 128, 0.9)"; /* Light background */
+        navbar.style.background = " #D4AF37"; /* Light background */
         navbar.style.boxShadow = "0px 2px 10px rgba(0, 0, 0, 0.1)"; /* Optional shadow */
     } else {
         navbar.style.background = "rgba(255, 255, 255, 0)"; /* Transparent */
